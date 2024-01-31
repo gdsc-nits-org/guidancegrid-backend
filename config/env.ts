69ec53dfb45f0a94433cc7ae1d418714ab6b5145
Jaydeep Das <jaydeepjd.8914@gmail.com>
2024-01-31 22:48:49 +0530

    send verification link as per env

--- config/env.ts
+++ config/env.ts
@@ -8,6 +8,7 @@ const envSchema = z.object({
     PORT: z.coerce.number().default(4000),
     DATABASE_URL_DEV: z.string(),
     DATABASE_URL_PROD: z.string(),
+    GUIDANCE_GRID_CLIENT_URI: z.string(),
     JWT_SIGNING_KEY: z.string(),
     AWS_ACCESS_KEY_ID: z.string(),
     AWS_SECRET_ACCESS_KEY: z.string(),
--- src/controllers/auth/verifyMail.ts
+++ src/controllers/auth/verifyMail.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import env from "config";
 import * as Interfaces from "../../interfaces";
 import * as Utils from "../../utils";
 
@@ -14,12 +15,58 @@ export const verifyMail: Interfaces.Controllers.Async = async (
     try {
         const validatedEmail = verifyEmailBody.parse(req.body);
         const token = Utils.Auth.signUp.genMailToken(validatedEmail.email);
+        let verificationLink;
+        if (env.NODE_ENV === "dev") {
+            verificationLink = `http://localhost:3000/create-user?token=${token}`;
+        } else if (env.NODE_ENV === "prod") {
+            verificationLink = `${env.GUIDANCE_GRID_CLIENT_URI}/create-user?token=${token}`;
+        }
         await Utils.Email.sendMail({
             subject: "Verify Email Address: Guidance Grid",
             body: `
-                <b>
-                    Click on this link to verify email address: <a href="http://localhost:3000/create-user?token=${token}">Verify Email</a>
-                </b>
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+              <style>
+                body {
+                    font-family: Arial, sans-serif;
+                    margin: 0;
+                    padding: 0;
+                    background-color: #f4f4f4;
+                }
+                .container {
+                    max-width: 600px;
+                    margin: auto;
+                    padding: 20px;
+                    background-color: #fff;
+                    border-radius: 8px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                }
+                h1 {
+                  color: #333;
+                }
+                p {
+                  color: #666;
+                }
+                a {
+                  color: #007BFF;
+                  text-decoration: none;
+                }
+              </style>
+            </head>
+            <body>
+              <div class="container">
+                <h1>Verify Email ID for Guidance Grid</h1>
+                <p>Someone wants to create an account on Guidance Grid using this email address (Hopefully it is you).</p>
+                <p>Click here to verify your mail.</p>
+                <p><a href="${verificationLink}">Verify Mail</a></p>
+                <p>Or paste this link directly in your browser: ${verificationLink}</p>
+                <p>If you didn't request this, you can safely ignore this email.</p>
+
+                <p>Best regards,<br>Guidance Grid Team</p>
+              </div>
+            </body>
+            </html>
             `,
             toaddress: validatedEmail.email,
         });
