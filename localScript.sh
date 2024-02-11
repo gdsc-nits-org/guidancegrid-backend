08eeccbcb4fb037b581dd138f31237ad04625fba
Priyabrat <prybrt06@gmail.com>
2024-02-11 11:45:54 +0530

    database migration

--- localScript.sh
+++ localScript.sh
@@ -5,8 +5,6 @@ echo "Pulling from git..."
 git pull origin dev
 echo "Installing dependencies..."
 pnpm install
-echo "Generating prisma client..."
-pnpm prisma generate
 echo "Migrating database..."
 pnpm prisma migrate dev
 
